4a5955e27bc6811585e3a2d46e68c0da1917ad3c
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2024-02-13 16:50:59 +0200

    add method PATCH

--- src/js/u-patch.js
+++ src/js/u-patch.js
@@ -1,1 +1,36 @@
 const BASE_URL = 'http://localhost:3000';
+
+function updateBook(book, bookId) {
+  const options = {
+    method: 'PATCH',
+    Headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(book),
+  };
+
+  return fetch(`${BASE_URL}/books/${bookId}`, options);
+}
+
+updateBook(
+  {
+    title: 'Clear JS The best time',
+  },
+  2
+);
+
+updateBook(
+  {
+    title: 'Great time',
+    author: 'no name',
+  },
+  3
+);
+
+updateBook(
+  {
+    title: 'Css',
+    rating: 7,
+  },
+  4
+);
